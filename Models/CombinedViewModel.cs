4ae5b0a9d5931857a3a5f1588d162bf05e9c505e
Nguyen Kim Hoang Anh <anhh20906@gmail.com>
2024-08-14 12:38:40 +0700

    Main

---
+++ Models/CombinedViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace NguyenKimHoangAnh.Context
+{
+    public class CombinedViewModel
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
